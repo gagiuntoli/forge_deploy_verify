5f222461357ba508054a40a5bad5a8ea42391cd9
Guido Giuntoli <gagiuntoli@gmail.com>
2022-10-21 16:25:00 +0200

    Adding a second contract as an example

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -2,11 +2,20 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
+import "../src/Counter.sol";
+import "../src/Counter1.sol";
 
 contract CounterScript is Script {
     function setUp() public {}
 
     function run() public {
-        vm.broadcast();
+        // vm.broadcast();
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        Counter counter = new Counter();
+        Counter1 counter1 = new Counter1();
+
+        vm.stopBroadcast();
     }
 }
